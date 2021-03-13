90ced57427786df8f9d0d1072345ac235d61ef7d
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-03-13 16:08:19 +0900

    update: アクセストークン取得処理つい

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import { GenericObject } from "next-auth/_utils";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,7 +8,60 @@ export default NextAuth({
     Providers.GitHub({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
+      scope: "repo read:org",
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    /**
+     * @param  {object} user     User object
+     * @param  {object} account  Provider account
+     * @param  {object} profile  Provider profile
+     * @return {boolean|string}  Return `true` to allow sign in
+     *                           Return `false` to deny access
+     *                           Return `string` to redirect to (eg.: "/unauthorized")
+     */
+    async signIn(user, account, profile) {
+      console.log("signIn!");
+      return true;
+    },
+    /**
+     * @param  {string} url      URL provided as callback URL by the client
+     * @param  {string} baseUrl  Default base URL of site (can be used as fallback)
+     * @return {string}          URL the client will be redirect to
+     */
+    async redirect(url, baseUrl) {
+      console.log("redirect!");
+      return url.startsWith(baseUrl) ? url : baseUrl;
+    },
+    /**
+     * @param  {object}  token     Decrypted JSON Web Token
+     * @param  {object}  user      User object      (only available on sign in)
+     * @param  {object}  account   Provider account (only available on sign in)
+     * @param  {object}  profile   Provider profile (only available on sign in)
+     * @param  {boolean} isNewUser True if new user (only available on sign in)
+     * @return {object}            JSON Web Token that will be saved
+     */
+    async jwt(token, user, account, profile, isNewUser) {
+      console.log("jwt!");
+      // Add access_token to the token right after signin
+      if (account?.accessToken) {
+        token.accessToken = account.accessToken;
+      }
+      return token;
+    },
+    /**
+     * @param  {object} session      Session object
+     * @param  {object} token        User object    (if using database sessions)
+     *                               JSON Web Token (if not using database sessions)
+     * @return {object}              Session that will be returned to the client
+     */
+    async session(session, token) {
+      console.log("session!");
+      console.log("type", typeof(token));
+      // Add property to session, like an access_token from a provider.
+      session.accessToken = (token.accessToken as GenericObject).accessToken;
+      return session;
+    },
+  },
 });
