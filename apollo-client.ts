8ad5d8b078228f0c359097c3f201551e6d969be7
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-03-14 15:09:34 +0900

    add: apollo-client追加

---
+++ apollo-client.ts
@@ -0,0 +1,11 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  headers: {
+    'Authorization': process.env.GITHUB_PERSONAL_ACCESSTOKEN ?? "",
+  },
+  cache: new InMemoryCache(),
+});
+
+export default client;
