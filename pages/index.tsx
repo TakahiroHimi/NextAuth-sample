8cc108975b5897c89d94e5f3a20be9e0a1a8c456
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-03-19 12:59:54 +0900

    update: 不要な処理を削除・Loading表示追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,20 @@
 import { signIn, signOut, useSession } from "next-auth/client";
-import client from "../apollo-client";
-import {
-  GetRepositoryDocument,
-  GetRepositoryQueryVariables,
-  Repository,
-} from "../graphql/queries/types.d";
 
-type Props = {
-  repository: Repository;
-};
-
-export default function Page({ repository }: Props) {
+export default function Page() {
   const [session, loading] = useSession();
-  console.log("Client", repository?.issues);
 
   return (
     <>
       {!session && (
         <>
-          Not signed in <br />
-          <button onClick={() => signIn()}>Sign in</button>
+          {loading ? (
+            <>Loading ...</>
+          ) : (
+            <>
+              Not signed in <br />
+              <button onClick={() => signIn()}>Sign in</button>
+            </>
+          )}
         </>
       )}
       {session && (
@@ -30,30 +25,6 @@ export default function Page({ repository }: Props) {
           <button onClick={() => signOut()}>Sign out</button>
         </>
       )}
-
-      {repository?.name}
-      {/* {repository?.issues.edges?.map((issue) => {
-        return issue?.node?.title;
-      })} */}
     </>
   );
-}
-
-export async function getServerSideProps() {
-  const { data: data } = await client.query({
-    query: GetRepositoryDocument,
-    variables: {
-      repositoryOwner: "octocat",
-      repositoryName: "Hello-World",
-      issuesFirst: 10,
-    } as GetRepositoryQueryVariables,
-  });
-
-  console.log("SSR", JSON.stringify(data));
-
-  return {
-    props: {
-      repository: data as Repository,
-    },
-  };
 }
