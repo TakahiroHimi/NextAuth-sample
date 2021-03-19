03ebd488160ff8033ffd1d23c4c78e78bf888ea0
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-03-19 13:23:23 +0900

    add: repositoryページ追加

---
+++ pages/repository.tsx
@@ -0,0 +1,32 @@
+import { signIn, signOut, useSession } from "next-auth/client";
+
+const Page = () => {
+  const [session, loading] = useSession();
+
+  return (
+    <>
+      {!session && (
+        <>
+          {loading ? (
+            <>Loading ...</>
+          ) : (
+            <>
+              Not signed in <br />
+              <button onClick={() => signIn()}>Sign in</button>
+            </>
+          )}
+        </>
+      )}
+      {session && (
+        <>
+          Signed in as <img src={session.user.image ?? ""} width="50px" />{" "}
+          {session.user.name} <br />
+          AccessToken : {session.accessToken} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Page;
